450a20096c4c8d61fabe1ebbb40dfe3ad858a43e
Mamadou Ben TRAORE <tmadben@gmail.com>
2021-01-27 08:20:43 +0000

    JSON payload structure review

--- consultationsExternes.js
+++ consultationsExternes.js
@@ -28,22 +28,86 @@ var jsonPayload = {
   "period": period,
   "orgUnit": orgUnit,
   "dataValues": [
-	{ "dataElement": "uwne8XcNsWR", "categoryOptionCombo": "PkyhEETzeVq", "value": "10", "comment": "" },
-    { "dataElement": "uwne8XcNsWR", "categoryOptionCombo": "yDODsYEbH1n", "value": "20", "comment": "" },
-	{ "dataElement": "uwne8XcNsWR", "categoryOptionCombo": "B1e6KGksbyE", "value": "30", "comment": "" },
-	{ "dataElement": "uwne8XcNsWR", "categoryOptionCombo": "cJZk4vnmox8", "value": "40", "comment": "" },
-	{ "dataElement": "jJtCxRd0dOJ", "categoryOptionCombo": "PkyhEETzeVq", "value": "50", "comment": "" },
-    { "dataElement": "jJtCxRd0dOJ", "categoryOptionCombo": "yDODsYEbH1n", "value": "60", "comment": "" },
-	{ "dataElement": "jJtCxRd0dOJ", "categoryOptionCombo": "B1e6KGksbyE", "value": "70", "comment": "" },
-	{ "dataElement": "jJtCxRd0dOJ", "categoryOptionCombo": "cJZk4vnmox8", "value": "80", "comment": "" },
-	{ "dataElement": "pfcaAuPitFB", "categoryOptionCombo": "PkyhEETzeVq", "value": "90", "comment": "" },
-    { "dataElement": "pfcaAuPitFB", "categoryOptionCombo": "yDODsYEbH1n", "value": "100", "comment": "" },
-	{ "dataElement": "pfcaAuPitFB", "categoryOptionCombo": "B1e6KGksbyE", "value": "110", "comment": "" },
-	{ "dataElement": "pfcaAuPitFB", "categoryOptionCombo": "cJZk4vnmox8", "value": "120", "comment": "" },
-	{ "dataElement": "bHoCQhb4cGr", "categoryOptionCombo": "PkyhEETzeVq", "value": "130", "comment": "" },
-    { "dataElement": "bHoCQhb4cGr", "categoryOptionCombo": "yDODsYEbH1n", "value": "140", "comment": "" },
-	{ "dataElement": "bHoCQhb4cGr", "categoryOptionCombo": "B1e6KGksbyE", "value": "150", "comment": "" },
-	{ "dataElement": "bHoCQhb4cGr", "categoryOptionCombo": "cJZk4vnmox8", "value": "160", "comment": "" },
+	{   "dataElement": "uwne8XcNsWR", 
+		"categoryOptionCombo": "PkyhEETzeVq", 
+		"value": "10", 
+		"comment": ""
+	},
+	{	"dataElement": "uwne8XcNsWR", 
+		"categoryOptionCombo": "yDODsYEbH1n", 
+		"value": "20", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "uwne8XcNsWR", 
+		"categoryOptionCombo": "B1e6KGksbyE",
+		"value": "30", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "uwne8XcNsWR", 
+		"categoryOptionCombo": "cJZk4vnmox8", 
+		"value": "40", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "jJtCxRd0dOJ", 
+		"categoryOptionCombo": "PkyhEETzeVq", 
+		"value": "50", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "jJtCxRd0dOJ", 
+		"categoryOptionCombo": "yDODsYEbH1n", 
+		"value": "60", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "jJtCxRd0dOJ", 
+		"categoryOptionCombo": "B1e6KGksbyE", 
+		"value": "70", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "jJtCxRd0dOJ", 
+		"categoryOptionCombo": "cJZk4vnmox8", 
+		"value": "80", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "pfcaAuPitFB", 
+		"categoryOptionCombo": "PkyhEETzeVq", 
+		"value": "90", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "pfcaAuPitFB", 
+		"categoryOptionCombo": "yDODsYEbH1n", 
+		"value": "100", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "pfcaAuPitFB", 
+		"categoryOptionCombo": "B1e6KGksbyE", 
+		"value": "110", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "pfcaAuPitFB", 
+		"categoryOptionCombo": "cJZk4vnmox8", 
+		"value": "120", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "bHoCQhb4cGr", 
+		"categoryOptionCombo": "PkyhEETzeVq", 
+		"value": "130", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "bHoCQhb4cGr", 
+		"categoryOptionCombo": "yDODsYEbH1n", 
+		"value": "140", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "bHoCQhb4cGr", 
+		"categoryOptionCombo": "B1e6KGksbyE", 
+		"value": "150", 
+		"comment": "" 
+	},
+	{ 	"dataElement": "bHoCQhb4cGr", 
+		"categoryOptionCombo": "cJZk4vnmox8", 
+		"value": "160", 
+		"comment": "" 
+	}
   ]
 };
 
